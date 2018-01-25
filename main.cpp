14a8d8b7247385f9316859efb83987e8a3b46373
Seth Kasmann <sakasmann1@cougars.ccis.edu>
2018-01-25 11:05:57 -0600

    Removed unnecessary code + comments. Removed the init function and moved the initializer calls into main.

--- main.cpp
+++ main.cpp
@@ -7,57 +7,17 @@
 #include <string>
 #include <sstream>
 #include "search.h"
-//#include "src/book.c"
 #include "test.h"
 #include "perft.h"
 #include "zobrist.h"
 #include "evaluation.h"
 #include "uci.h"
 
-void init()
+int main(int argc, char* argv[])
 {
     mg_init();
     Zobrist::init();
     bb_init();
-
-    // initialize opening book database
-
-    /*
-    strcpy(TOERFILE,"db/tourbook.bin");   
-    strcpy(BOOKFILE,"db/mainbook.bin");    
-    INITIALIZE();
-    if (ERROR)
-    {
-        printf("Something went wrong, error-code %d",ERROR);
-        exit(1);
-    }
-    */
-}
-
-// Currently unused function to test opening book
-/*
-void choose(State & s)
-{
-    strcpy(EPD, s.get_EPD());
-    std::cout << s.get_EPD() << '\n';
-    std::cout << s;
-    FIND_OPENING();
-    if (ERROR)
-    {
-        printf("Something went wrong, error-code %d",ERROR);
-    }
-    else
-    {
-        printf("Move: %s-%s\nList: ",FROM,TO);
-        for (int x=0; x<AZ; x++) printf("%c%c-%c%c ",FROM1[x],FROM2[x],TO1[x],TO2[x]);
-        printf("\n\n");
-    }
-}
-*/
-#include <array>
-int main(int argc, char* argv[])
-{
-    init();
     uci();
     return 0;
 } 
